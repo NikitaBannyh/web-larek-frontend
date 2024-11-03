887c6ced08615016677d84ce0bb3d1fa407c440b
Nikkita <nikbann@bk.ru>
2024-11-03 16:56:47 +0300

    add types

---
+++ src/types/index.ts
@@ -0,0 +1,84 @@
+export type EventName = string | RegExp;
+export type Subscriber = Function;
+export type EmitterEvent = {
+    eventName: string,
+    data: unknown
+};
+
+export interface IEvents {
+    on<T extends object>(event: EventName, callback: (data: T) => void): void;
+    off<T extends object>(event: EventName, callback: (data: T) => void): void;
+    emit<T extends object>(event: string, data?: T): void;
+
+  }
+
+export type ApiListResponse<Type> = {
+  total: number,
+  items: Type[]
+};
+
+export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
+
+
+export interface ILarekApi {
+    getCardsList: () => Promise<ICard[]>;
+    orderProducts: (order: IOrder) => Promise<IOrderSuccess>
+  }
+
+export interface IAppState {
+    catalog: ICard[];
+    basket: ICard[];
+    preview: string | null;
+    delivery: IOrdersDelivery | null;
+    contact: IOrdersContacts | null;
+    order: IOrder | null;
+  }
+
+export interface IBasket {
+    items: HTMLElement[];
+    total: number;
+}
+
+export interface ICard {
+  id: string,
+  description: string,
+  image: string,
+  title: string,
+  category: string,
+  price: number | null,
+  count?: string,
+  buttonText? : string;
+}
+
+export interface ISuccess {
+  image: string,
+  title: string,
+  description: string,
+  total: number | null,
+}
+
+export interface IPage {
+  counter: number;
+  catalog: HTMLElement[];
+  locked: boolean;
+}
+
+export interface IOrdersDelivery {
+  payment: string,
+  address: string,
+}
+
+export interface IOrdersContacts {
+  email: string,
+  phone: string,
+}
+
+export interface IOrder extends IOrdersDelivery, IOrdersContacts {
+  total: number | null,
+  items: string[],
+}
+
+export interface IOrderSuccess {
+  id: string,
+  total: number | null,
+}
